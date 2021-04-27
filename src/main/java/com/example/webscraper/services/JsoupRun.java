5ded27cf9eabd7200b776152b02920bbd93c651d
Emil-PC <emil.ankerstjerne@gmail.com>
2021-04-27 14:47:57 +0200

    Changed en JsoupRun

--- src/main/java/com/example/webscraper/services/JsoupRun.java
+++ src/main/java/com/example/webscraper/services/JsoupRun.java
@@ -31,9 +31,13 @@ public class JsoupRun implements CommandLineRunner {
 
 //        Element e = doc.selectFirst("table#hnmain");
         Elements e = doc.select("a.storylink");
+        Elements el = doc.select("span.rank");
+        Elements els = doc.select("span.age");
         for(int i = 0; i<e.size(); i++){
+            System.out.println("Rank: " + el.eq(i).text());
             System.out.println("Titel: " + e.eq(i).text());
             System.out.println("Link: " + e.eq(i).attr("href"));
+            System.out.println(els.eq(i).text());
             System.out.println("\n");
         }
 
